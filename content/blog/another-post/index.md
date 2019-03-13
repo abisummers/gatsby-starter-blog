767d3345fbb84f44653b755eff675acaea9fae26
abisummers <hello@abisummers.com>
2019-03-13 15:04:35 +0100

    updated blog with 'personal' info

---
+++ content/blog/another-post/index.md
@@ -0,0 +1,6 @@
+---
+title: Another post!
+date: "2019-03-13T22:12:03.284Z"
+---
+
+I like learning news things
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
+    title: `First Gatsby Blog`,
+    author: `Abi Summers`,
+    description: `Learning what Gatsby can do.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `abijsummers`,
     },
   },
   plugins: [
